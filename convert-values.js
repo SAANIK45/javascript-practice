840d572b405324e3eedd219c7c12681a0002ad86
Samiulahmedanik <84468942+SAANIK45@users.noreply.github.com>
2021-08-04 15:36:26 +0600

    inch to feet

---
+++ convert-values.js
@@ -0,0 +1,11 @@
+//Inch to feet
+
+function inchToFeet(value){
+    var feet = value/12;
+    return feet;
+}
+var inch = 140;
+var result = inchToFeet(inch);
+console.log(result);
+
+//Inch to feet
