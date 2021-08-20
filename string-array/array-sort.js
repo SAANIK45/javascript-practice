ae6849f9571a414785298def1975ff2cf3910100
Samiulahmedanik <84468942+SAANIK45@users.noreply.github.com>
2021-08-20 14:07:08 +0600

    update files

---
+++ string-array/array-sort.js
@@ -0,0 +1,6 @@
+const numbers = [2,3,5,1,7,8,6,9];
+const sortedNumbers = numbers.sort();
+console.log(sortedNumbers);
+
+const name = ['Anik','Balam','Famin','Dipjol','Choity'];
+console.log(name.sort());
---
+++ string-array/try-catch.js
@@ -0,0 +1,10 @@
+//try Catch 
+
+const name =  'ami';
+try{
+    name = 'tmi';
+}
+catch(error){
+    console.log(error);
+}
+console.log(name);
